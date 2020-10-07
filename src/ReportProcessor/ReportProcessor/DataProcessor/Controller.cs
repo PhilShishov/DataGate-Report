0d08f3085f16f5084aa8f902c0d06b61c5940b7b
Philip Shishov <philshishov@gmail.com>
2020-10-07 19:09:01 +0200

    Prepare new security checks before uploading csv data to DB

--- src/ReportProcessor/ReportProcessor/DataProcessor/Controller.cs
+++ src/ReportProcessor/ReportProcessor/DataProcessor/Controller.cs
@@ -17,6 +17,20 @@
                 hasPassedCheck = false;
             }
 
+            // Second security check: Nav date matching expected date from internal DB
+            //if (shareClass == null)
+            //{
+            //    logger.Error(string.Format(ErrorMessages.InvalidIsin, isin, currency));
+            //    hasPassedCheck = false;
+            //}
+
+            // Third security check: Aggregated sf Aum and share sum of AuM have same result
+            //if (shareClass == null)
+            //{
+            //    logger.Error(string.Format(ErrorMessages.InvalidIsin, isin, currency));
+            //    hasPassedCheck = false;
+            //}
+
             return hasPassedCheck;
         }
     }
